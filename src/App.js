a76a86e347ef5de203de2e293c90785bfe311306
Matt Fisher <mfisher@MacBook-Prox.local>
2020-07-14 15:19:46 -0500

    use cognito login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./styles.css";
-import GoogleBtn from "./GoogleBtn";
+
+const loginUrl =
+  "https://mattcfish.auth.us-west-2.amazoncognito.com/login?response_type=token&client_id=7hlfqe4ofiv4c7ovst7krl4c3v&redirect_uri=https://mattcfish.com";
+
 
 export default function App() {
+  let content;
+  if (window.idToken !== null) {
+    content = <div> test </div>
+  } else {
+    content = (
+      <button onClick={ event => (window.location.href = loginUrl)}> Login with Google</button>
+    )
+  }
   return (
     <div className="App">
-      <h1>Fisher Test</h1>
-      <GoogleBtn />
+      <h3>Welcome</h3>
+      {content}
     </div>
   );
 }
